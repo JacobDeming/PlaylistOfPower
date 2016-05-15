bf46a4ee1a521cfe08ff1b30cb40c268b13fda3b
Jacob Deming <Jacob Deming>
2016-05-15 18:23:04 -0400

    Eye Color Now Changes

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -11,8 +11,8 @@ app.post('/api/song',function(req,res){
 		var hue='#'+data[0].hex;
 		var color=tinycolor(hue);
 		if(color.isDark()==true){
-			text='#000000';}
-		else{text='#FFFFFF';}
+			text='#FFFFFF';}
+		else{text='#000000';}
 	Song.create({title:req.body.title,artist:req.body.artist,link:req.body.link,eye_color:req.body.color,hue:hue,opp_hue:text,power_name:req.body.name,power_type:req.body.type}).then(function(song){
 			res.redirect("/");})})})}
 
