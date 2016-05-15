7e06e4aa89f4ccffefeb7365c44655f1a03fcb28
Jacob Deming <Jacob Deming>
2016-05-15 18:17:56 -0400

    Eye Color Now Changes

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -1,21 +1,18 @@
 var Song=require("../models/SQLConnection.js");
 var path=require('path');
 var colourlovers=require('colourlovers');
+var tinycolor = require("tinycolor2");
 
 module.exports=function(app){
 app.post('/api/song',function(req,res){
 	colourlovers.get('/colors',{keywords:req.body.color,keywordExact:1,numResults:1},function(err,data){
 		if(err) throw err;
+		var text="";
 		var hue='#'+data[0].hex;
-		var opp='#'+invertColor(data[0].hex);
+		var color=tinycolor(hue);
+		if(color.isDark()==true){
+			text='#000000';}
+		else{text='#FFFFFF';}
 	Song.create({title:req.body.title,artist:req.body.artist,link:req.body.link,eye_color:req.body.color,hue:hue,opp_hue:opp,power_name:req.body.name,power_type:req.body.type}).then(function(song){
 			res.redirect("/");})})})}
 
-function invertColor(hex){
-    var color = hex;
-    color = color.substring(1);
-    color = parseInt(color, 16);
-    color = 0xFFFFFF ^ color;
-    color = color.toString(16);
-    color = ("000000" + color).slice(-6);
-    return color;}
