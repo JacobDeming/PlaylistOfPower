880feef19553f036f16af5bdf89c270f569de0ae
Jacob Deming <Jacob Deming>
2016-05-15 18:39:10 -0400

    Eye Color Column Complete

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -5,7 +5,20 @@ var tinycolor = require("tinycolor2");
 
 module.exports=function(app){
 app.post('/api/song',function(req,res){
-	colourlovers.get('/colors',{keywords:req.body.color,keywordExact:1,numResults:1},function(err,data){
+	if(req.body.color.split(" - ").length>=2){
+		getColorText=req.body.color.split(" - ");
+		colourlovers.get('/colors',{keywords:getColorText[0],keywordExact:1,numResults:1},function(err,data){
+			if(err) throw err;
+			var text="";
+			var hue='#'+data[0].hex;
+			var color=tinycolor(hue);
+			if(color.isDark()==true){
+				text='#FFFFFF';}
+			else{text='#000000';}
+		Song.create({title:req.body.title,artist:req.body.artist,link:req.body.link,eye_color:getColorText[1],hue:hue,opp_hue:text,power_name:req.body.name,power_type:req.body.type}).then(function(song){
+				res.redirect("/");})})}
+	else{
+		colourlovers.get('/colors',{keywords:req.body.color,keywordExact:1,numResults:1},function(err,data){
 		if(err) throw err;
 		var text="";
 		var hue='#'+data[0].hex;
@@ -13,6 +26,6 @@ app.post('/api/song',function(req,res){
 		if(color.isDark()==true){
 			text='#FFFFFF';}
 		else{text='#000000';}
-	Song.create({title:req.body.title,artist:req.body.artist,link:req.body.link,eye_color:req.body.color,hue:hue,opp_hue:text,power_name:req.body.name,power_type:req.body.type}).then(function(song){
-			res.redirect("/");})})})}
+	Song.create({title:req.body.title,artist:req.body.artist,link:req.body.link,hue:hue,opp_hue:text,power_name:req.body.name,power_type:req.body.type}).then(function(song){
+			res.redirect("/");})})}})}
 
